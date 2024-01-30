29117b55ba8478ce1edc7ee7bc0a96bd7c5ced55
dinelly43 <valdirdinelly@hotmail.comgit config --global user.namedinelly43git config --global user.email valdirdinelly@hotmail.com>
2024-01-30 12:07:41 -0300

    fomr in js

---
+++ js/index.js
@@ -0,0 +1,33 @@
+let logo = document.querySelector('.logo');
+criarBtn.onclick = () => {
+    overlay.classList.add('show');
+    formCriar.classList.add('show');
+}
+
+fecharBtn.onclick = () => {
+    overlay.classList.remove('show');
+    formCriar.classList.remove('show');
+}
+
+overlay.onclick = () => {
+    overlay.classList.remove('show');
+    formCriar.classList.remove('show');
+}
+
+formCriar.onsubmit =  () => {
+    event.preventDefault();
+    // alert(nome.value+' '+capacidade.value);
+    overlay.classList.remove('show');
+    formCriar.classList.remove('show');
+    listTeams.innerHTML = '';
+    listTeams.innerHTML = `
+                    <li>
+                        <h4>Nome do Time <box-icon name="show"></box-icon></h4>
+                        <h1>0 <span>/ 0</span></h1>
+                        <div class="actions">
+                            <button>Adicionar</button>
+                            <button><box-icon name="trash"></box-icon></button>
+                        </div>
+                    </li>
+    `;
+}
